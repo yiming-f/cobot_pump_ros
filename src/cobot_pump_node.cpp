3e5f1ded92b4721d547a1ce59b61d00dfee459d4
David Russell <el16dmcr@leeds.ac.uk>
2022-10-12 15:33:45 +0100

    Created two services, one turns the pump on at a set pressure, one turns the pump off

--- src/cobot_pump_node.cpp
+++ src/cobot_pump_node.cpp
@@ -5,27 +5,96 @@
 #include <iostream>
 #include <thread>
 
-// #include <franka/robot.h>
-// #include <franka/vacuum_gripper.h>
-// #include <franka/exception.h>
+#include <franka/robot.h>
+#include <franka/vacuum_gripper.h>
+#include <franka/exception.h>
 
 #include "ros/ros.h"
-#include "cobot_pump_ros/vacuumService.h"
+#include "cobot_pump_ros/startPump.h"
+#include "cobot_pump_ros/stopPump.h"
 
 using namespace std;
 
-bool add(cobot_pump_ros::vacuumService::Request &req, cobot_pump_ros::vacuumService::Response &res){
-  res.sum = req.a + req.b;
-  return true;
+// TODO - make this ip changeable when you create the node, i.e via arguments
+// Instantiate vacuum gripper with our ip address
+franka::VacuumGripper vacuum_gripper("192.168.130.1");
+
+//------------------------------------------------------------
+//
+//          Turns pump on at set pressure
+//
+//------------------------------------------------------------
+bool startPump(cobot_pump_ros::startPump::Request &req, cobot_pump_ros::startPump::Response &res){
+  
+    vacuum_gripper.vacuum(500, std::chrono::milliseconds(5000));
+    return true;
+}
+
+//------------------------------------------------------------
+//
+//                      Turns pump off
+//
+//------------------------------------------------------------
+bool stopPump(cobot_pump_ros::stopPump::Request &req, cobot_pump_ros::stopPump::Response &res){
+    vacuum_gripper.stop();
+    return true;
 }
 
 int main(int argc, char **argv){
 
-  cout << "Hello, world!, this is the node with services" << endl;
-  ros::init(argc, argv, "cobot_pump");
-  ros::NodeHandle n;
+    cout << "Hello, world!, Initial vacuum testing "<< endl;
 
-  ros::ServiceServer service = n.advertiseService("add_two_ints", add);
+    // Print a vacuum gripper state.
+    franka::VacuumGripperState vacuum_gripper_state = vacuum_gripper.readOnce();
+    std::cout << "Initial vacuum gripper state: " << vacuum_gripper_state << std::endl;
 
-  ros::spin();
+
+
+    // Establish an initial connection to the franka panda
+    
+
+    // Create the ros node (cobot_pump)
+    ros::init(argc, argv, "cobot_pump");
+    ros::NodeHandle n;
+
+    // Advertise services for the vacuum gripper
+    ros::ServiceServer service = n.advertiseService("startPump", startPump);
+    ros::ServiceServer service2 = n.advertiseService("stopPump", stopPump);
+
+    ros::spin();
 }
+
+
+  //   
+
+  //   try {
+  //   // Print a vacuum gripper state.
+  //   franka::VacuumGripperState vacuum_gripper_state = vacuum_gripper.readOnce();
+  //   std::cout << "Initial vacuum gripper state: " << vacuum_gripper_state << std::endl;
+
+  //   // Vacuum the object.
+  //   if (!vacuum_gripper.vacuum(500, std::chrono::milliseconds(1000))) {
+  //     std::cout << "Failed to vacuum the object." << std::endl;
+  //     return -1;
+  //   }
+
+  //   vacuum_gripper_state = vacuum_gripper.readOnce();
+  //   std::cout << "Vacuum gripper state after applying vacuum: " << vacuum_gripper_state
+  //             << std::endl;
+
+  //   // Wait 3s and check afterwards, if the object is still grasped.
+  //   std::this_thread::sleep_for(std::chrono::duration<double, std::milli>(3000));
+
+  //   vacuum_gripper_state = vacuum_gripper.readOnce();
+  //   if (!vacuum_gripper_state.in_control_range) {
+  //     std::cout << "Object lost." << std::endl;
+  //     return -1;
+  //   }
+
+  //   std::cout << "Vacuumed object, will release it now." << std::endl;
+  //   vacuum_gripper.dropOff(std::chrono::milliseconds(1000));
+  // } catch (franka::Exception const& e) {
+  //   vacuum_gripper.stop();
+  //   std::cout << e.what() << std::endl;
+  //   return -1;
+  // }
